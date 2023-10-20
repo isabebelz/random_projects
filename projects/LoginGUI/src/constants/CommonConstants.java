c227135bcecd2dea0a38da4a1af9afeb87a026c2
Isabel Oliveira <isabel.01oa@outlook.com>
2023-10-20 12:23:17 -0300

    username and password fields added and configured

--- projects/LoginGUI/src/constants/CommonConstants.java
+++ projects/LoginGUI/src/constants/CommonConstants.java
@@ -2,6 +2,7 @@ package constants;
 
 public class CommonConstants {
     public final static String APP_NAME = "Login App";
-
     public final static int[] FRAME_SIZE = {400, 400};
+    public final static int TEXTFIELD_SIZE = 15;
+
 }
--- projects/LoginGUI/src/gui/LoginGUI.java
+++ projects/LoginGUI/src/gui/LoginGUI.java
@@ -3,6 +3,11 @@ package gui;
 import constants.CommonConstants;
 
 import javax.swing.*;
+import java.awt.*;
+
+/** LoginGUI é uma subclasse da classe JFrame. Ela herda todos os recursos e
+ * funcionalidades da classe JFrame e pode adicionar ou modificar comportamentos
+ * específicos conforme necessário. */
 
 public class LoginGUI extends JFrame {
     public LoginGUI() {
@@ -11,13 +16,59 @@ public class LoginGUI extends JFrame {
         setSize(CommonConstants.FRAME_SIZE[0], CommonConstants.FRAME_SIZE[1]);
         setLocationRelativeTo(null);
 
+        // OBS: setLocationRelativeTo(null) é chamado após setSize(CommonConstants.FRAME_SIZE[0], CommonConstants.FRAME_SIZE[1]) pq a janela é centralizada com base no tamanho definido anteriormente.
+
         addGUIComponents();
     }
 
     public void addGUIComponents() {
         SpringLayout springLayout = new SpringLayout();
+
         JPanel loginPanel = new JPanel();
+
         loginPanel.setLayout(springLayout);
 
+        // username
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+
+        JTextField usernameText = new JTextField(CommonConstants.TEXTFIELD_SIZE);
+
+        loginPanel.add(usernameLabel);
+        loginPanel.add(usernameText);
+
+        springLayout.putConstraint(SpringLayout.NORTH, usernameLabel, 130, SpringLayout.NORTH, loginPanel);
+        springLayout.putConstraint(SpringLayout.WEST, usernameLabel, 70, SpringLayout.WEST, loginPanel);
+        springLayout.putConstraint(SpringLayout.NORTH, usernameText, 130, SpringLayout.NORTH, loginPanel);
+        springLayout.putConstraint(SpringLayout.WEST, usernameText, 135, SpringLayout.WEST, loginPanel);
+
+        // password
+
+        JLabel passwordLabel = new JLabel("Password: ");
+        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+
+        JPasswordField passwordText = new JPasswordField(CommonConstants.TEXTFIELD_SIZE);
+
+        springLayout.putConstraint(SpringLayout.NORTH, passwordLabel, 160, SpringLayout.NORTH, loginPanel);
+        springLayout.putConstraint(SpringLayout.WEST, passwordLabel, 70, SpringLayout.WEST, loginPanel);
+        springLayout.putConstraint(SpringLayout.NORTH, passwordText, 160, SpringLayout.NORTH, loginPanel);
+        springLayout.putConstraint(SpringLayout.WEST, passwordText, 135, SpringLayout.WEST, loginPanel);
+
+
+        loginPanel.add(passwordLabel);
+        loginPanel.add(passwordText);
+
+        // login button
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setFont(new Font("Dialog", Font.BOLD, 15));
+
+        springLayout.putConstraint(SpringLayout.NORTH, loginButton, 190, SpringLayout.NORTH, loginPanel);
+        springLayout.putConstraint(SpringLayout.WEST, loginButton, 140, SpringLayout.WEST, loginPanel);
+
+        loginPanel.add(loginButton);
+
+        this.getContentPane().add(loginPanel);
     }
 }
