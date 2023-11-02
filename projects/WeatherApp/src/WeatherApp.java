19e1775b1081fcd9ca9bc59eefe62c0b4e9f6e11
Isabel Oliveira <isabel.01oa@outlook.com>
2023-11-02 16:38:34 -0300

    establishing http connection with the api

--- projects/WeatherApp/src/WeatherApp.java
+++ projects/WeatherApp/src/WeatherApp.java
@@ -3,9 +3,12 @@
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class WeatherApp {
     // fetch weather data for given location
@@ -26,16 +29,60 @@ public class WeatherApp {
         try {
             // call api and get a response
             HttpURLConnection connection = fetchAPIResponse(urlString);
+
+            // check response status
+            // 200 means successful connection
+            if(connection.getResponseCode() != 200) {
+                System.out.println("Error: Could not connect to API");
+                return null;
+            } else {
+                // store the API results
+                StringBuilder resultJson = new StringBuilder();
+                Scanner sc = new Scanner(connection.getInputStream());
+
+                // read and store the resulting json data into our string builder
+                while (sc.hasNext()) {
+                    resultJson.append(sc.nextLine());
+                }
+
+                sc.close();
+
+                // close url connection
+                connection.disconnect();
+
+                // parse the JSON string into a JSON obj
+                JSONParser parser = new JSONParser();
+                JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+
+                // get the list of location data the API generated from the location name
+                JSONArray locationData = (JSONArray) resultsJsonObj.get("results");
+                return locationData;
+            }
+
+
         }catch (Exception e) {
             e.printStackTrace();
         }
 
-        privat static HttpURLConnection fetchAPIResponse(String urlString) {
+        private static HttpURLConnection fetchAPIResponse(String urlString) {
             try{
                 // attemp to create connection
                 URL url = new URL(urlString);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // set request method to get
+                connection.setRequestMethod("GET");
+
+                // connect to our API
+                connection.connect();
+                return connection;
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
+            // could not make connection
+            return null;
         }
     }
 }
